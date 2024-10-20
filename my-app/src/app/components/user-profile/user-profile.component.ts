275c35851e81b74da51d310811fb2ad0c2528dae
Parv Kumra <parvkumra2003@gmail.com>
2024-10-20 18:28:28 +0530

    some advances

--- my-app/src/app/components/user-profile/user-profile.component.ts
+++ my-app/src/app/components/user-profile/user-profile.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { UserProfilecompComponent } from '../user-profilecomp/user-profilecomp.component';
+import { DropdownComponent } from '../dropdown/dropdown.component';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [UserProfilecompComponent],
+  imports: [UserProfilecompComponent,DropdownComponent],
   templateUrl: './user-profile.component.html',
   styleUrl: './user-profile.component.css'
 })
